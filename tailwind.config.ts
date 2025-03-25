840aa342aa57c2d620edaa9604fe375e2c5e6f66
Adewale shittu <sheriffhardewale@gmail.com>
2025-03-25 17:55:45 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,12 @@ export default {
   theme: {
     extend: {
       colors: {
+        brandgreen: '#174023',
+        brandgray: '#2F2A33',
+        lightgray: '#17402326',
+        lightgreen: '#056D22',
+        lightorange: '#F8F1EA',
+        lightergreen: '#154029B2',
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
